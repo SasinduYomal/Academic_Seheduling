5756013f91bdacaf477a3fa607469811c1f28359
DinujayaMendis <dinujaya.vimu@gmail.com>
2025-04-27 21:50:16 +0530

    Post_Sharing
    Create a handleFileChange function

--- frontend/src/components/Search/SkillSharingPosts.js
+++ frontend/src/components/Search/SkillSharingPosts.js
@@ -8,6 +8,12 @@ import {
 } from "@mui/material";
 
 function SkillSharingPosts() {
-    
+    const [files, setFiles] = useState([]);
+    const [description, setDescription] = useState("");
+
+    const handleFileChange = (e) => {
+        const selectedFiles = Array.from(e.target.files).slice(0, 3); // Limit to 3
+        setFiles(selectedFiles);
+    };
 }
     
