1f04d2a60c5af599f6319646eef329d5ff33f7ac
Hiruna Adithya <132162381+HirunaAdithya@users.noreply.github.com>
2025-05-16 11:01:05 +0530

    Update Comment.tsx

--- frontend/src/components/Comment.tsx
+++ frontend/src/components/Comment.tsx
@@ -22,8 +22,7 @@ const Comment = ({ text, onDelete, onEdit }) => {
                 />
             ) : (
                 <p className="flex-1 text-sm break-words">{text}</p>
-            )}
-            <div className="flex gap-2 justify-end w-full">
+            )}>
                 {isEditing ? (
                     <button
                         onClick={handleSave}
