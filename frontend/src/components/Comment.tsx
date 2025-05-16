a2d2b23159da64e65b94a011d47b37649646a3b7
Hiruna Adithya <132162381+HirunaAdithya@users.noreply.github.com>
2025-05-16 11:05:20 +0530

    Update Comment.tsx

--- frontend/src/components/Comment.tsx
+++ frontend/src/components/Comment.tsx
@@ -2,7 +2,8 @@ import {useState} from "react";
 
 const Comment = ({ text, onDelete, onEdit }) => {
     const [isEditing, setIsEditing] = useState(false);
-    
+    const [editedText, setEditedText] = useState(text);
+
     const handleSave = () => {
         if (editedText.trim() !== "") {
             onEdit(editedText.trim());
